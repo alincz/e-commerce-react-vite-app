52b2495a9c12052cf8f30dc9672111e7b3d9cfb2
Alin Cazacu <109979223+alincz@users.noreply.github.com>
2024-06-24 14:53:14 +0300

    Update ProductDetails.jsx

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -4,19 +4,15 @@ import { CartContext } from "../contexts/CartContext";
 import { ProductContext } from "../contexts/ProductContext";
 
 const ProductDetails = () => {
-  //get de product id from the url
+
   const { id } = useParams();
   const { products } = useContext(ProductContext);
   const { addToCart } = useContext(CartContext);
 
-  //iau produsul bazat pe id
-  //products este state-ul care are ca valori
-  //in interiorul lui datele produselor
   const product = products.find((item) => {
     return item.id === parseInt(id);
   });
 
-  //if product is not found
   if (!product) {
     return (
       <section className="h-screen flex justify-center items-center">
@@ -25,19 +21,15 @@ const ProductDetails = () => {
     );
   }
 
-  //destructurare de obiecte ca sa iau datele produsului
   const { title, price, description, image } = product;
   return (
     <section className="pt-32 pb-12 lg:py-32 h-screen flex items-center">
       <div className="container mx-auto">
-        {/* image and text wrapper */}
-        <div className="flex flex-col lg:flex-row items-center">
-          {/* image */}
+      <div className="flex flex-col lg:flex-row items-center">
+     
           <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
             <img className="max-w-[200px] lg:max-w-sm" src={image} alt="" />
           </div>
-
-          {/* text */}
           <div className="flex-1 text-center lg:text-left">
             <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0">
               {title}
